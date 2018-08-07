5fb965079ada523a92f0f59ad2a8e28489b74eca
xiaobuding <1942304871@qq.com>
2018-08-07 15:40:12 +0800

    nav finish

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -22,7 +22,16 @@
         <div class="register">注册</div>
       </div>
     </div>
-    <div class="banner"></div>
+    <div class="banner">
+      <div class="find">
+        <div class="find-icon">此处是一张喇叭图片</div>
+        <div class="find-img">报告组织,发现喵星人的足迹!!!</div>
+      </div>
+      <div class="publish">
+        <div class="publish-button">发布喵星人</div>
+        <div class="publish-img">此处一张喵星人合影</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,18 +49,73 @@ export default {
     .nav
       width: 100%
       height:80px
-      padding:5px 300px
-      background:green
-      .nav-left
-       display:inline-block
-       width:240px
-       background:red
-       .nav-left-logo
-        width:50px
-        height:50px
-        border-radius:25px
-        background:orange
-      .nav-left-title
-       float:right
-       background:yellow
+      padding:5px 200px
+    .nav-left
+     display:inline-block
+     width:240px
+     .nav-left-logo
+      margin:10px 5px
+      display:inline-block
+      width:50px
+      height:50px
+      border-radius:25px
+      background:orange
+    .nav-left-title
+     position:relative
+     top:20px
+     left:0
+     right:160px
+     bottom:0
+     color:orange
+     display:inline-block
+     float:right
+    .nav-list
+     margin-left:30px
+     position:relative
+     top:-30px
+     right:0
+     left:0
+     bottom:0
+     height:80px
+     display:inline-block
+     .nav-list-first
+       color: #ffffff
+       background:orange
+    .nav-list ul
+      height:80px
+      display :flex
+    .nav-list ul li
+     font-size:16px
+     padding:0 40px
+     line-height:60px
+  .nav-right
+    margin-left:40px
+    position:relative
+    top:-30px
+    right:0
+    left:0
+    bottom:0
+    height:80px
+    display:inline-block
+    padding:0 10px
+   .login ,.register
+    margin:5px 20px
+    width:70px
+    height:25px
+    display:inline-block
+    color:#fff
+    border-radius:5px
+    line-height:25px
+    text-align:center
+    background:orange
+   .banner
+    width:100%
+    height:300px
+    padding:5px 200px
+    background:green
+  .find-icon
+    width:70px
+    height:70px
+    text-align:center
+    background:#eee
 </style>
