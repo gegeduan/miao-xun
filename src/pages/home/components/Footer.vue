f001f9619fe7c10186a5d5a24020a262dddbad5b
xiaobuding <1942304871@qq.com>
2018-08-09 18:42:59 +0800

    首页footer更新

--- src/pages/home/components/Footer.vue
+++ src/pages/home/components/Footer.vue
@@ -1,9 +1,25 @@
 <template>
   <div class="footer-wrapper">
      <div class="footer">
-       <div class="footer-logo"></div>
-       <div class="footer-contact"></div>
-       <div class="weixin"></div>
+       <div class="footer-left">
+         <div class="logo">LOGO</div>
+       </div>
+       <div class="footer-contact">
+         <div class="box">
+           <h3>联系我们CONTACT</h3>
+           <p>E-mail:xxxxxxxxxx@xx.com</p>
+           <p>QQ:xxxxxxxxxx</p>
+           <p class="copy">Copyright 2018-2018 公益猫 版权所有 .</p>
+
+       </div>
+       </div>
+       <div class="footer-right">
+         <div class="footer-right-box">
+           <h3>微信公众号</h3>
+           <div class="weixin"></div>
+           <p>扫一扫,我们一起养猫 .</p>
+       </div>
+       </div>
      </div>
   </div>
 </template>
@@ -23,8 +39,38 @@ export default {
     width:1280px
     margin:0 auto
     display:flex
-    .footer-logo,.footer-contact,.weixin
+    .footer-left,.footer-contact
+      padding-top:10px
       width: 33.3%
       height:200px
-      border:1px solid #666
+      .logo
+       width:150px
+       height:150px
+       font-size:40px
+       color:#807570
+       margin:10px auto
+       text-align:center
+       line-height:150px
+       background:#cfa28c
+    .footer-contact
+      color:#fff
+      padding-left:60px
+      .box
+       margin:10px auto
+       .copy
+        margin-top:80px
+     .footer-right
+       color:#fff
+       .footer-right-box
+         margin-left:60px
+        .footer-right-box h3
+         padding-left:20px
+        .weixin
+          width:150px
+          height:150px
+          font-size:40px
+          color:#807570
+          text-align:center
+          line-height:150px
+          background:#cfa28c
 </style>
