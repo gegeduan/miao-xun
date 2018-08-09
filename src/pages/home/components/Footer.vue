8a73a9959a1b22e318c373ae5f6828dd812ac67f
xiaobuding <1942304871@qq.com>
2018-08-09 17:40:46 +0800

    首页内容部分结束

--- src/pages/home/components/Footer.vue
+++ src/pages/home/components/Footer.vue
@@ -1,7 +1,11 @@
 <template>
- <div>
-   footer
- </div>
+  <div class="footer-wrapper">
+     <div class="footer">
+       <div class="footer-logo"></div>
+       <div class="footer-contact"></div>
+       <div class="weixin"></div>
+     </div>
+  </div>
 </template>
 
 <script>
@@ -11,5 +15,16 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-
+  .footer-wrapper
+   width:1920px
+   height:200px
+   background:#fc8d59
+   .footer
+    width:1280px
+    margin:0 auto
+    display:flex
+    .footer-logo,.footer-contact,.weixin
+      width: 33.3%
+      height:200px
+      border:1px solid #666
 </style>
