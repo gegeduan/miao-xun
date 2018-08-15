75ca013240986e8292437e26a0282e0bb2aa6c2d
xiaobuding <1942304871@qq.com>
2018-08-15 14:30:06 +0800

    页面样式修改

---
+++ src/pages/community/Community.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <community-header></community-header>
+  </div>
+</template>
+
+<script>
+import CommunityHeader from './components/Header'
+export default {
+  name: 'Community',
+  components: {
+    CommunityHeader
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import List from '@/pages/list/List'
+import Community from '@/pages/community/Community'
 Vue.use(Router)
 
 export default new Router({
@@ -15,6 +16,11 @@ export default new Router({
       path: '/list',
       name: 'List',
       component: List
+    },
+    {
+      path: '/community',
+      name: 'Community',
+      component: Community
     }
   ]
 })
