90f1f6eaca13e0fba21716ad373bf9911a14ef39
xiaobuding <1942304871@qq.com>
2018-08-16 16:25:20 +0800

    社区页面底部添加完成

--- src/pages/community/Community.vue
+++ src/pages/community/Community.vue
@@ -4,6 +4,7 @@
     <community-find></community-find>
     <community-search></community-search>
     <community-blog></community-blog>
+    <home-footer></home-footer>
   </div>
 </template>
 
@@ -12,13 +13,15 @@ import CommunityHeader from './components/Header'
 import CommunityFind from './components/Find'
 import CommunitySearch from './components/Search'
 import CommunityBlog from './components/Blog'
+import HomeFooter from '@/common/Footer'
 export default {
   name: 'Community',
   components: {
     CommunityHeader,
     CommunityFind,
     CommunitySearch,
-    CommunityBlog
+    CommunityBlog,
+    HomeFooter
   }
 }
 </script>
